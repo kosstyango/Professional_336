152f54ffbdd3f1492fe4c4d2e73b19ebb614d32a
Konstantin Barnaev <konstantin.barnaev@gmail.com>
2022-04-18 20:31:36 +0300

    Лаб раб3_БарнаевКС

--- src/HomeWorkBarnaev.java
+++ src/HomeWorkBarnaev.java
@@ -1,35 +1,13 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class HomeWorkBarnaev {
-    //Программа угадай число
-
     public static void main(String[] args) {
-
-        int compNumber = random();
-
-        boolean f = true;
-        while (f == true) {
-            if (scan() == compNumber) {
-                System.out.println("Вы выиграли");
-                System.out.println("ПОЗДРАВЛЯЮ!!!");
-                f = false;
-            } else {
-                System.out.println("Это неправильный ответ :(");
-                System.out.println("Вы проиграли");
-            }
-        }
+        //дан массив [2, 3, 4, 5]
+        int[] list = {2, 3, 4, 5};
+        System.out.println("Произведение элементов массива равно " + multArray(list));
     }
-        public static int random() {
-            Random newRandom = new Random();
-            int randoNumber = newRandom.nextInt(11) + 1;
-            return randoNumber;
-        }
 
-        public static int scan(){
-            System.out.println("Введите число от 1 до 10");
-            Scanner scanner = new Scanner(System.in);
-            int humanNumber = Integer.parseInt(scanner.nextLine());
-            return humanNumber;
-        }
+    public static int multArray(int list[]) {
+        int s = 1;
+        for (int i : list) s*=i;
+        return s;
     }
+}
